89508cbfa3d15f6c01c2aa3c259e66951d40b310
e.lascano <emilas5678@gmail.com>
2026-03-28 14:42:27 -0400

    Complete main file

---
+++ app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from app.db.database import Base, engine
+from app.routes import clothing
+
+app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
+
+app.include_router(clothing.router)
+
+@app.get("/")
+def read_root():
+    return {"message": "Alure backend is running"}
